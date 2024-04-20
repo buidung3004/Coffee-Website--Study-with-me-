86ad4323ea23a8b1dd4a0fa473430d44707d827f
buidung3004 <125304045+buidung3004@users.noreply.github.com>
2024-04-20 19:08:28 +0700

    Validate data

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -119,6 +119,7 @@ module.exports.create = async(req,res) => {
 
 // [POST] /admin/products/create
 module.exports.createPost = async(req,res) => {
+
     req.body.price = parseInt(req.body.price)
     req.body.discountPercentage = parseInt(req.body.discountPercentage)
     req.body.stock = parseInt(req.body.stock)
@@ -129,8 +130,10 @@ module.exports.createPost = async(req,res) => {
     } else {
         req.body.position = parseInt(req.body.position)
     }
+    if (req.file) {
+        req.body.thumbnail = `/uploads/${req.file.filename}`
+    }
 
-    req.body.thumbnail = `/uploads/${req.file.filename}`
     const product = new Product(req.body)
     await product.save()
 
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -6,6 +6,9 @@ const upload = multer({ storage: storageMulter() })
 
 const controller = require("../../controllers/admin/product.controller");
 
+const validate = require("../../validates/admin/product.validate");
+
+
 router.get("/", controller.index);
 // thay đổi trạng thái sản phẩm
 router.patch("/change-status/:status/:id", controller.changeStatus);
@@ -16,6 +19,9 @@ router.delete("/delete/:id", controller.deleteItem);
 
 router.get("/create",controller.create)
 
-router.post("/create", upload.single("thumbnail"), controller.createPost)
+router.post("/create",
+    upload.single("thumbnail"), 
+    validate.createPost,
+    controller.createPost)
 
 module.exports = router
