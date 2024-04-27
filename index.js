ad9461d704870b64a13ce76c804e4979a00a8586
buidung3004 <125304045+buidung3004@users.noreply.github.com>
2024-04-28 00:05:21 +0700

    TinyMCE

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path')
 const methodOverride = require('method-override')
 // const bodyParser = require("body-parser") // ex4.18 ver kh cần cài thêm, đã fix
 const flash = require("express-flash")
@@ -22,13 +23,19 @@ const port = process.env.PORT;
 app.use(methodOverride('_method'));
 // parse application/x-www/form-urlencoded
 app.use(express.urlencoded({ extended: false}))
+
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "pug");
+
+
 // Flash
 app.use(cookieParser('manchesterunited'));
 app.use(session({ cookie: { maxAge: 60000 }}));
 app.use(flash());
 
-app.set("views", `${__dirname}/views`);
-app.set("view engine", "pug");
+// TinyMCE
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
+
 
 // App Local Variables
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
---
+++ public/admin/js/tinymce-config.js
@@ -0,0 +1,5 @@
+tinymce.init({
+    selector: 'textarea.textarea-mce',
+    plugins: "image textcolor",
+    toolbar: "forecolor backcolor"
+});
