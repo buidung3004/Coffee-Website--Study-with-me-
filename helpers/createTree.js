4f7e7ba8e6793e1eb2e9d7c7111d3c8a6933bc83
buidung3004 <125304045+buidung3004@users.noreply.github.com>
2024-05-01 22:21:29 +0700

    STT

--- helpers/createTree.js
+++ helpers/createTree.js
@@ -1,8 +1,11 @@
+let count = 0;
 createTree = (arr, parentId = "") => {
     const tree = []
     arr.forEach((item) => {
         if(item.parent_id === parentId) {
+            count++ 
             const newItem = item
+            newItem.index = count
             const children = createTree(arr, item.id)
             if (children.length > 0) {
                 newItem.children = children
@@ -15,6 +18,7 @@ createTree = (arr, parentId = "") => {
 
 
 module.exports.tree = (arr, parentId = "") => {
+    count = 0
     const tree = createTree(arr, parentId="")
     return tree
 }
