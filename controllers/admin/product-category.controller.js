09789a77048a8456b901d6daba9df17cd0cf0cfd
buidung3004 <125304045+buidung3004@users.noreply.github.com>
2024-05-01 22:06:45 +0700

    Create Tree Category

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,26 +1,40 @@
 const ProductCategory = require("../../models/product-category.model")
 
 const systemConfig = require("../../config/system")
-
+const createTree = require("../../helpers/createTree")
 
 // [GET] /admin/products-category
 module.exports.index =  async(req, res) => {   
     let find = {
         deleted: false
     };
+
+
     const records = await ProductCategory.find(find)
-    
+
+    const newRecords = createTree.tree(records)    
 
     res.render("admin/pages/products-category/index", {
         pageTitle: "Danh mục sản phẩm",
-        records: records
+        records: newRecords
     });
 };
 
 // [GET] /admin/products-category/create
 module.exports.create =  async(req, res) => {    
+    let find = {
+        deleted: false
+    }
+
+
+    const records = await ProductCategory.find(find)  
+    // console.log(records)
+    const newRecords = createTree.tree(records)
+
+    console.log(newRecords)
     res.render("admin/pages/products-category/create", {
         pageTitle: "Tạo danh mục",
+        records: newRecords
     });
 };
 
