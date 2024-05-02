80355aab73a1c09c8e81712eb1be8d785113ce87
buidung3004 <125304045+buidung3004@users.noreply.github.com>
2024-05-02 23:32:14 +0700

    Update TinyMCE

--- public/admin/js/tinymce-config.js
+++ public/admin/js/tinymce-config.js
@@ -1,5 +1,34 @@
 tinymce.init({
     selector: 'textarea.textarea-mce',
     plugins: "image textcolor",
-    toolbar: "forecolor backcolor"
+    toolbar: "forecolor backcolor",
+    file_picker_callback: (cb, value, meta) => {
+        const input = document.createElement('input');
+        input.setAttribute('type', 'file');
+        input.setAttribute('accept', 'image/*');
+    
+        input.addEventListener('change', (e) => {
+            const file = e.target.files[0];
+    
+            const reader = new FileReader();
+            reader.addEventListener('load', () => {
+            /*
+              Note: Now we need to register the blob in TinyMCEs image blob
+              registry. In the next release this part hopefully won't be
+              necessary, as we are looking to handle it internally.
+            */
+            const id = 'blobid' + (new Date()).getTime();
+            const blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+            const base64 = reader.result.split(',')[1];
+            const blobInfo = blobCache.create(id, file, base64);
+            blobCache.add(blobInfo);
+    
+            /* call the callback and populate the Title field with the file name */
+            cb(blobInfo.blobUri(), { title: file.name });
+            });
+            reader.readAsDataURL(file);
+        });
+    
+        input.click();
+    },
 });
