48e90516a1c8ee7693fdc31d779188b31f4f566c
buidung3004 <125304045+buidung3004@users.noreply.github.com>
2024-05-03 08:49:16 +0700

    Add category for product

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,9 +1,12 @@
 const Product = require("../../models/product.model");
+const ProductCategory = require("../../models/product-category.model")
+
 
 const systemConfig = require("../../config/system")
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search")
 const paginationHelper = require("../../helpers/pagination")
+const createTree = require("../../helpers/createTree")
 
 // [GET] /admin/products
 module.exports.index =  async(req, res) => {    
@@ -123,8 +126,16 @@ module.exports.deleteItem = async (req, res) => {
 // [GET] /admin/products/create
 
 module.exports.create = async(req,res) => {
+    let find = {
+        deleted:false
+    }
+    const category = await ProductCategory.find(find)
+
+    const newCategory = createTree.tree(category)
+
     res.render("admin/pages/products/create", {
         pageTitle:"Add new product",
+        category:newCategory
     })
 }
 
--- models/product.model.js
+++ models/product.model.js
@@ -6,6 +6,10 @@ mongoose.plugin(slug)
 const productSchema = new mongoose.Schema(
     {
         title: String,
+        productCategoryId:{
+            type:String,
+            default:""
+        },
         description: String,
         price: Number,
         discountPercentage: Number,
