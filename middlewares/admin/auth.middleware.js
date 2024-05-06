3a6ee31955082804b5bf7146ded3758daab0ab62
buidung3004 <125304045+buidung3004@users.noreply.github.com>
2024-05-06 15:47:37 +0700

    Authorization

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,5 +1,6 @@
 const systemConfig = require("../../config/system")
 const Account = require("../../models/account.model")
+const Role = require("../../models/role.model")
 
 module.exports.requireAuth = async (req,res,next) =>{
 
@@ -7,14 +8,18 @@ module.exports.requireAuth = async (req,res,next) =>{
         res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
 
     } else {
-        const user = await Account.findOne({token:req.cookies.token})
+        const user = await Account.findOne({token:req.cookies.token}).select("-password")
         if (!user) {
             res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
         } else {
+            const role = await Role.findOne({
+                _id: user.role_id
+            }).select("title permissions")
+
+            res.locals.role = role
+            res.locals.user = user
             next()
         }
     }
-
-
 
 }
