2bee6baff17a6516eee877b37fa8eb7c424167e1
buidung3004 <125304045+buidung3004@users.noreply.github.com>
2024-05-12 01:10:39 +0700

    Tao model Order

---
+++ models/order.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+
+const orderSchema = new mongoose.Schema(
+    {
+        // user_id: String,
+        cart_id:String,
+        userInfo: {
+            fullName: String,
+            phone: String,
+            address: String
+        },
+        products: [
+            {
+                product_id:String,
+                price: Number,
+                discountPercentage: Number,
+                quantity: Number
+            }
+        ]
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Order = mongoose.model("Order", orderSchema, "orders");
+
+
+module.exports = Order;
