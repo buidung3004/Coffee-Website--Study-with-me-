840a3b110f1edda442f0ea6c62082e861d89de99
buidung3004 <125304045+buidung3004@users.noreply.github.com>
2024-05-11 16:06:48 +0700

    trang gio hang

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,6 +1,34 @@
 const Cart =  require("../../models/cart.model")
+const Product = require('../../models/product.model')
 
 const productsHelper = require("../../helpers/product") 
+
+// [GET] /cart/
+module.exports.index = async (req, res) => {
+    const cartId = req.cookies.cartId
+    const cart = await Cart.findOne({
+        _id: cartId
+    })
+    if (cart.products.length > 0) {
+        for(const item of cart.products) {
+            const productId = item.product_id
+            const productInfo = await Product.findOne({
+                _id:productId
+            })
+            productInfo.newPrice = productsHelper.priceNewProduct(productInfo)
+            item.productInfo = productInfo
+            item.totalPrice = item.quantity*productInfo.newPrice
+        }
+    }
+    cart.totalPrice = cart.products.reduce((sum,item) =>sum+item.totalPrice,0)
+
+    res.render("client/pages/cart/index", {
+        pageTitle: "Giỏ hàng",
+        cartDetail: cart,
+
+    });
+
+}
 
 // [POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
--- routes/client/cart.route.js
+++ routes/client/cart.route.js
@@ -3,6 +3,10 @@ const router = express.Router()
 
 const controller = require("../../controllers/client/cart.controller");
 
+router.get("/", controller.index);
+
 router.post("/add/:productId", controller.addPost);
+
+
     
 module.exports = router
